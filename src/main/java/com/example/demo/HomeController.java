1259fb62734d47103e57c9ea24ff875f01d92de3
hamnaasif5 <hamna.asif245@gmail.com>
2019-08-23 11:50:47 -0400

    ds

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.jws.WebParam;
 import javax.persistence.SequenceGenerators;
@@ -32,13 +30,30 @@ public class HomeController {
     }
 
 
-        @PostMapping("process")
-        public String processForm (@Valid Message message, BindingResult result)
-        {
-          if (result.hasErrors()){
-              return "messageform";
-          }
-          messageRepository.save(message);
-          return "redirect:/";
+    @PostMapping("process")
+    public String processForm(@Valid Message message, BindingResult result) {
+        if (result.hasErrors()) {
+            return "messageform";
+        }
+        messageRepository.save(message);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showMessage (@PathVariable("id") long id, Model model){
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model){
+        model.addAttribute("message" , messageRepository.findById(id).get());
+        return "messageform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String deleteMessage(@PathVariable("id") long id, Model model){
+        messageRepository.deleteById(id);
+        return "redirect:/";
     }
 }
+
